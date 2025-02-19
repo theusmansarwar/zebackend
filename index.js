3cf0cee8f9d5f905a779201f540506748dc33b53
Muhammad Usman Sarwar <105904144+theusmansarwar@users.noreply.github.com>
2025-02-19 23:29:13 +0500

    update env

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const port=process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 
-app.use("/api", userRouter)
+app.use("/", userRouter)
 app.use('/uploads', express.static(path.join(__dirname, 'uploads'))); // Serve static files
 
 
