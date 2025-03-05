a7ef5cbfe9dab18de519709beb024038f1c6d66f
Muhammad Usman Sarwar <105904144+theusmansarwar@users.noreply.github.com>
2025-03-05 10:34:26 +0500

    update

--- Routes/serviceRoutes.js
+++ Routes/serviceRoutes.js
@@ -1,14 +1,7 @@
 const express = require("express");
+const { deleteService, deleteMultipleServices, updateService, getServiceById, getAllLiveServices, getAllServices, createService } = require("../Controller/serviceController");
 const router = express.Router();
-const {
-  createService,
-  getAllServices,
-  getServiceById,
-  updateService,
-  deleteService,
-  deleteMultipleServices,
-  getAllLiveServices
-} = require("../Controllers/servicesController");
+
 
 router.post("/create", createService);      // Create a new service
 router.get("/view", getAllServices); 
@@ -16,6 +9,6 @@ router.get("/live", getAllLiveServices);      // Get all services
 router.get("/view/:id", getServiceById);   // Get a single service by ID
 router.put("/update/:id", updateService);    // Update a service
 router.delete("/delete/:id", deleteService); 
-router.delete("/delete", deleteService); 
+router.delete("/delete", deleteMultipleServices); 
 
 module.exports = router;
