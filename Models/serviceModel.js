61755dff31a9d7913fd4341641807ffce6345180
Muhammad Usman Sarwar <105904144+theusmansarwar@users.noreply.github.com>
2025-03-06 14:03:23 +0500

    update

--- Models/serviceModel.js
+++ Models/serviceModel.js
@@ -21,6 +21,7 @@ const ServiceSchema = new mongoose.Schema({
   published: { type: Boolean, default: false },
   services: [
     {
+      _id: { type: mongoose.Schema.Types.ObjectId, auto: true },
       image: { type: String, required: true }, 
       title: { type: String, required: true },
       description: { type: String, required: true },
@@ -29,6 +30,7 @@ const ServiceSchema = new mongoose.Schema({
   ],
   benefits: [
     {
+      _id: { type: mongoose.Schema.Types.ObjectId, auto: true },
       title: { type: String, required: true },
       img: { type: String, required: true }, 
       description: { type: String, required: true },
@@ -37,6 +39,7 @@ const ServiceSchema = new mongoose.Schema({
   ],
   process: [
     {
+      _id: { type: mongoose.Schema.Types.ObjectId, auto: true },
       image: { type: String, required: true },
       title: { type: String, required: true },
       description: { type: String, required: true },
@@ -45,12 +48,13 @@ const ServiceSchema = new mongoose.Schema({
   ],
   pricing: [
     {
+      _id: { type: mongoose.Schema.Types.ObjectId, auto: true },
       name: { type: String, required: true }, 
       price: { type: String, required: true },
       title: { type: String, required: true },
       published: { type: Boolean, default: false },
       services: [
-        {
+        { 
           name: { type: String, required: true },
         },
       ],
