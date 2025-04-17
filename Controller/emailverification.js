c20cf5356f49a73cf03c82b36f675f67aceb9a19
Muhammad Usman Sarwar <105904144+theusmansarwar@users.noreply.github.com>
2025-04-17 15:28:57 +0500

    custome email

--- Controller/emailverification.js
+++ Controller/emailverification.js
@@ -15,7 +15,7 @@ const transporter = nodemailer.createTransport({
   
     // ✅ 1. Email to the Customer
     const customerMailOptions = {
-      from: "noreply@zemalt.com",
+      from: `"PlutoSec" <noreply@zemalt.com>`,
       to: email,
       subject: `Thank You for Reaching Out – Zemalt Pvt Ltd`,
       html: `
