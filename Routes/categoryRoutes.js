ce428a77f40b285dab0a07d7a4cbe8f1df028b3f
Muhammad Usman Sarwar <105904144+theusmansarwar@users.noreply.github.com>
2025-09-03 11:20:15 +0500

    FAQs commit 1

--- Routes/categoryRoutes.js
+++ Routes/categoryRoutes.js
@@ -15,7 +15,7 @@ router.post("/add",authMiddleware, addCategory);
 router.put("/update/:id",authMiddleware, updateCategory);
 router.delete("/delete/:id",authMiddleware, deleteCategory);
 router.delete("/delete",authMiddleware, deleteAllCategories);
-router.get("/view",authMiddleware, viewCategory);
+router.get("/view", viewCategory);
 router.get("/live", liveCategory);
 
 module.exports = router;
