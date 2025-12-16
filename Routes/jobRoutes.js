1c6875347f87a3d16c07d00a368cdf8fbdbc6e56
Aleem Ahmad <ahmadaleem592@gmail.com>
2025-12-16 16:32:57 +0500

    updated files

--- Routes/jobRoutes.js
+++ Routes/jobRoutes.js
@@ -14,5 +14,5 @@ router.put("/update/:id", updateJob);
 router.get("/get/:id", getJobById);
 router.get("/view", viewJobs);
 router.get("/live", liveJobs);
-router.delete("/deleteAll", deleteAllJobs);
+router.delete("/delete", deleteAllJobs);
 module.exports = router;
